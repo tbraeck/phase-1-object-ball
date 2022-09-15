cf68cf5fd24e38bd526ce2eadc4145969c8b3c0b
Tate <tatebraeckel@gmail.com>
2022-09-14 19:31:30 -0600

    commit tree 724ba24d02acd3217c07ae981e4fa005ce29683e

---
+++ src/00-objectball.js
@@ -0,0 +1,146 @@
+const gameObject = [
+    homeTeam: {
+        teamName: " Brooklyn Nets",
+        colors: [Black, White],
+        players: {
+            p1: {
+                name: "Alan Anderson",
+                stats: {
+                    number: 0,
+                    shoe: 16,
+                    points: 22,
+                    rebounds: 12,
+                    assists: 12,
+                    steals: 3,
+                    blocks: 1,
+                    slamDunks: 1,
+                },
+            },
+            p2: {
+                name: "Reggie Evans",
+                stats: {
+                    number: 30,
+                    shoe: 14,
+                    points: 12,
+                    rebounds: 12,
+                    assists: 12,
+                    steals: 12,
+                    blocks: 12,
+                    slamDunks: 7,
+                },
+            },
+            p3: {
+                name: "Brook Lopez",
+                stats: {
+                    number: 11,
+                    shoe: 17,
+                    points: 17,
+                    rebounds: 19,
+                    assists: 10,
+                    steals: 3,
+                    blocks: 1,
+                    slamDunks: 15,
+                },
+            },
+            p4: {
+                name: "Mason Plumlee",
+                stats: {
+                    number: 1,
+                    shoe: 19,
+                    points: 26,
+                    rebounds: 12,
+                    assists: 6,
+                    steals: 3,
+                    blocks: 8,
+                    slamDunks: 5,
+                },
+            },
+            p5: {
+                name: "Jason Terry",
+                stats: {
+                    number: 31,
+                    shoe: 15,
+                    points: 19,
+                    rebounds: 2,
+                    assists: 2,
+                    steals: 4,
+                    blocks: 11,
+                    slamDunks: 1,
+                },
+            },
+        },
+    },
+    awayTeam: {
+        teamName: "Charlotte Hornets",
+        colors: Turquoise, Purple,
+        players: {
+            p1: {
+                name: "Jeff Adrien",
+                stats: {
+                    number: 4,
+                    shoe: 18,
+                    points: 10,
+                    rebounds: 1,
+                    assists: 1,
+                    steals: 2,
+                    blocks: 7,
+                    slamDunks: 2,
+                },
+            },
+            p2: {
+                name: "Bismak Biyombo",
+                stats: {
+                    number: 0,
+                    shoe: 16,
+                    points: 12,
+                    rebounds: 4,
+                    assists: 7,
+                    steals: 7,
+                    blocks: 15,
+                    slamDunks: 10,
+                },
+            },
+            p3: {
+                name: "DeSagna Diop",
+                stats: {
+                    number: 2,
+                    shoe: 14,
+                    points: 24,
+                    rebounds: 12,
+                    assists: 12,
+                    steals: 4,
+                    blocks: 5,
+                    slamDunks: 5,
+                },
+            },
+            p4: {
+                name: "Ben Gordon",
+                stats: {
+                    number: 8,
+                    shoe: 15,
+                    points: 33,
+                    rebounds: 3,
+                    assists: 2,
+                    steals: 1,
+                    blocks: 1,
+                    slamDunks: 0,
+                },
+            },
+            p5: {
+                name: "Brendan Haywood",
+                stats: {
+                    number: 33,
+                    shoe: 15,
+                    points: 6,
+                    rebounds: 12,
+                    assists: 12,
+                    steals: 22,
+                    blocks: 5,
+                    slamDunks: 12,
+                },
+            },
+        },
+    },
+]
+
+console.log(gameObject());
